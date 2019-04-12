b9482f9f53bd2a4965bea917b39160dbcb28ad1a
ESM Smart Solutions <esm.smart.solutions@outlook.com>
2019-04-12 12:14:11 -0400

    Add project files.

---
+++ LocalNotifications_iOS/ViewController.cs
@@ -0,0 +1,79 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using UserNotifications;
+
+namespace LocalNotifications_iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public bool alertsAllowed = false;
+        public int alertCount = 1;
+
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            UNUserNotificationCenter.Current.GetNotificationSettings((settings) => {
+                alertsAllowed = (settings.AlertSetting == UNNotificationSetting.Enabled);
+            });
+
+            BtnSendAlert.TouchUpInside += OnSendAlert;
+        }
+
+        private void OnSendAlert(object sender, EventArgs e)
+        {
+            // Get current notification settings
+            //UNUserNotificationCenter.Current.GetNotificationSettings(OnNotificationSettings);
+
+            if (alertsAllowed)
+            {
+                // Create the content of the Local Notification
+                UNMutableNotificationContent content = new UNMutableNotificationContent();
+                content.Title = txtNotificationTitle.Text;
+                content.Subtitle = txtNotificationSubTitle.Text;
+                content.Body = $"{txtNotificationBody.Text} with Notification Count of {alertCount}";
+                content.Badge = 1;
+
+                UNTimeIntervalNotificationTrigger trigger = UNTimeIntervalNotificationTrigger.CreateTrigger(15.0, false);
+
+                string requestID = $"SampleRequest{alertCount}";
+                UNNotificationRequest request = UNNotificationRequest.FromIdentifier(requestID, content, trigger);
+
+                lblAlertStatus.Text = "Sending Alert...";
+
+                UNUserNotificationCenter.Current.AddNotificationRequest(request, (err) =>
+                {
+                    if (err != null)
+                    {
+                        //this.lblAlertStatus.Text = "Error: Alert NOT Sent";
+                    }
+                    else
+                    {
+                        //this.lblAlertStatus.Text = "Alert Sent";
+                    }
+                });
+
+                alertCount++;
+            }
+            else
+                lblAlertStatus.Text = "Alerts NOT Allowed";
+        }
+
+        private void OnNotificationSettings(UNNotificationSettings settings)
+        {
+            alertsAllowed = (settings.AlertSetting == UNNotificationSetting.Enabled);
+            swtAlertsAllowed.On = alertsAllowed;
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ LocalNotifications_iOS/ViewController.designer.cs
@@ -0,0 +1,74 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace LocalNotifications_iOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton BtnSendAlert { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblAlertStatus { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISwitch swtAlertsAllowed { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtNotificationBody { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtNotificationSubTitle { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtNotificationTitle { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (BtnSendAlert != null) {
+                BtnSendAlert.Dispose ();
+                BtnSendAlert = null;
+            }
+
+            if (lblAlertStatus != null) {
+                lblAlertStatus.Dispose ();
+                lblAlertStatus = null;
+            }
+
+            if (swtAlertsAllowed != null) {
+                swtAlertsAllowed.Dispose ();
+                swtAlertsAllowed = null;
+            }
+
+            if (txtNotificationBody != null) {
+                txtNotificationBody.Dispose ();
+                txtNotificationBody = null;
+            }
+
+            if (txtNotificationSubTitle != null) {
+                txtNotificationSubTitle.Dispose ();
+                txtNotificationSubTitle = null;
+            }
+
+            if (txtNotificationTitle != null) {
+                txtNotificationTitle.Dispose ();
+                txtNotificationTitle = null;
+            }
+        }
+    }
+}
